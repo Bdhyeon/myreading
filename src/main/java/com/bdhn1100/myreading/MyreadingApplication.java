9aa30c6364c3a38c8c7d124ed2e45e93de77e307
Bdhyeon <bdhn1100@gmail.com>
2022-09-25 12:57:28 +0900

    new project

---
+++ src/main/java/com/bdhn1100/myreading/MyreadingApplication.java
@@ -0,0 +1,13 @@
+package com.bdhn1100.myreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyreadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdhn1100/myreading/MyreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdhn1100.myreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
